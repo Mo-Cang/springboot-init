5c82e55bc11a5126a0e335f850b0283f42ce28e7
MoCang <sdd515424@qq.com>
2023-10-15 14:01:37 +0800

    first init

---
+++ src/main/java/com/mocang/project/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.mocang.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mocang.project.model.entity.User;
+
+/**
+ * @Entity com.mocang.project.model.domain.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
